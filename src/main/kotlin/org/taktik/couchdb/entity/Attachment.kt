7de8903ca702ca67a2bac9b897508a6e8f0d1037
Luca Tremamunno <tremaluca96@gmail.com>
2025-04-18 11:32:43 +0200

    Proper attachment stubbing (#39)

--- src/main/kotlin/org/taktik/couchdb/entity/Attachment.kt
+++ src/main/kotlin/org/taktik/couchdb/entity/Attachment.kt
@@ -31,11 +31,15 @@ import com.fasterxml.jackson.annotation.JsonProperty
 data class Attachment(
         @JsonIgnore val id: String? = null,
         @field:JsonProperty("content_type") val contentType: String? = null,
-        @JsonIgnore val contentLength: Long = 0,
+        @JsonIgnore val contentLength: Long? = null,
         @field:JsonProperty("data") val dataBase64: String? = null,
         @field:JsonProperty("stub")
         val isStub: Boolean = false,
-        val revpos: Int = 0,
+        val revpos: Int? = null,
         val digest: String? = null,
         val length: Long? = null
-)
+) {
+    companion object {
+        val EmptyStub = Attachment(isStub = true)
+    }
+}
