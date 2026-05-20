22fef57ecb53c23580790891683f82d969b012b1
Vincenzo Pierro <vcp@icure.com>
2026-05-20 11:45:19 +0200

    Wip test

--- src/test/kotlin/org/taktik/couchdb/entity/ViewTest.kt
+++ src/test/kotlin/org/taktik/couchdb/entity/ViewTest.kt
@@ -6,10 +6,10 @@ import org.junit.jupiter.api.Assertions.*
 
 class ViewTest {
 
-    @Test
-    fun testEquals() {
-        assertEquals(View(map = "map = function (doc) { emit(1) }"), View(map = "function(doc) { emit(1) }"))
-        assertEquals(View(map = "function(doc) { emit(1) };\n"), View(map = "function(doc) { emit(1) }\n"))
-        assertNotEquals(View(map = "map = function(doc) { emit(2) }"), View(map = "function(doc) { emit(1) }"))
-    }
+	@Test
+	fun testEquals() {
+		assertEquals(View(map = "map = function (doc) { emit(1) }"), View(map = "function(doc) { emit(1) }"))
+		assertEquals(View(map = "function(doc) { emit(1) };\n"), View(map = "function(doc) { emit(1) }\n"))
+		assertNotEquals(View(map = "map = function(doc) { emit(2) }"), View(map = "function(doc) { emit(1) }"))
+	}
 }
